ef4dc080d4afd69548b6192d4ff5430e6e2dfbca
da99 <spam.i.hate@miniuni.com>
2025-11-05 08:43:35 -0600

    Development checkpoint.

--- run/main.go
+++ run/main.go
@@ -3,8 +3,11 @@ package run
 
 import (
 	"github.com/da99/cli.go/exit"
+	"os"
 	"os/exec"
 	"strings"
+	"bytes"
+	"bufio"
 )
 
 
@@ -15,4 +18,26 @@ func Lines(cmd_str string) []string {
 	exit.PrintError(o_err)
 
 	return strings.Split(strings.TrimSpace(string(output)), "\n")
+}
+
+// Returns a string slice of output from a command executed via exec.Command.
+func Cmd_Args(cmd string, args ...string) []string {
+	os_cmd := exec.Command(cmd, args...)
+	var out bytes.Buffer
+	os_cmd.Stdout = &out
+	os_cmd.Stderr = os.Stderr;
+	err := os_cmd.Run()
+	if err != nil {
+		exit.PrintError(err)
+	}
+	return Bytes_To_Lines(&out)
+}
+
+func Bytes_To_Lines(buf *bytes.Buffer) []string {
+	scanner := bufio.NewScanner(buf);
+	fin := []string{}
+	for scanner.Scan() {
+		fin = append(fin, scanner.Text())
+	}
+	return fin
 }
